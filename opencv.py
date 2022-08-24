23499103e9cb6c2893e845296afc6335256539e0
Dipu Sharma <sdipu231@gmail.com>
2022-08-24 21:19:52 +0530

    commit

--- opencv.py
+++ opencv.py
@@ -2,7 +2,11 @@
 import cv2
 import time
 import math
+import numpy as np
 import HandTrackingModule as htm
+# from ctypes import cast, POINTER
+# from comtypes import CLSCTX_ALL
+# from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 wCam, hCam = 1280, 720
 
@@ -13,6 +17,25 @@ pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
 
+# ____________________Below code use for Window os work only___________
+
+# devices = AudioUtilities.GetSpeakers()
+# interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+# volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+# volume.SetMasterVolumeLevel(-20.0, None)
+# print(volume.GetVolumeRange())
+# volRange = volume.GetVolumeRange()
+
+volRange = (-65, 0.0, 0.03125)
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+# ______________Above code use for Window os only work________
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
@@ -20,6 +43,7 @@ while True:
     if len(lmList[0]) != 0:
         # print(lmList[0][4], lmList[0][8])
 
+        # find value for two point of first finger and thumb
         x1, y1 = lmList[0][4][1], lmList[0][4][2]
         x2, y2 = lmList[0][8][1], lmList[0][8][2]
         # Find Two finger center value
@@ -32,10 +56,22 @@ while True:
         # Two finger center circle
         cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
-        length = math.hypot(x2 - x1, y2 -y1)
+        length = math.hypot(x2 - x1, y2 - y1)
+        # print(length)
+
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volBar = np.interp(length, [50, 300], [400, 150])
+        volPer = np.interp(length, [50, 300], [0, 100])
+
+        # for window only
+        # volume.SetMasterVolumeLevel(vol, None)
 
         if length < 25:
             cv2.circle(img, (cx, cy), 15, (255, 255, 255), cv2.FILLED)
+
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, f'Volume :{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 3)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
